7a2f64e4433572791dcee4ecd40165c375f8639f
Alex <alex.mani@student.moringaschool.com>
2025-03-21 19:27:43 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function hoursWorkedOnDate(targetDate) {
+    let inEvent = this.timeInEvents.find(e => e.date === targetDate);
+    let outEvent = this.timeOutEvents.find(e => e.date === targetDate);
+    if (!inEvent || !outEvent) return 0;
+    return (outEvent.hour - inEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(targetDate) {
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour;
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +59,10 @@ const allWagesFor = function () {
 
     return payable
 }
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(emp => emp.firstName === firstNameString) || null;
+}
 
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, emp) => total + allWagesFor.call(emp), 0);
+}
